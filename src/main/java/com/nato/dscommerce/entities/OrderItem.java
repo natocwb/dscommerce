dc098514c3fd009a5d49c72d693a34897bcc16f3
natalicio almeida <natoctba@hotmail.com>
2024-11-23 14:44:58 -0300

    OrderItem and Products,Categorys

---
+++ src/main/java/com/nato/dscommerce/entities/OrderItem.java
@@ -0,0 +1,28 @@
+package com.nato.dscommerce.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order_item")
+public class OrderItem {
+    @EmbeddedId
+    private OrderItemPK id = new OrderItemPK();
+    private Integer quantity;
+    private Double price;
+    public OrderItem() {
+    }
+    public OrderItem(Order order, Product product, Integer quantity, Double price) {
+        id.setOrder(order);
+        id.setProduct(product);
+        this.quantity = quantity;
+        this.price = price;
+    }
+    public Order getOrder() {
+        return id.getOrder();
+    }
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+}
