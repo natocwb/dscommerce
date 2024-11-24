ae47d9c1834d72d3bb7d1351c9c4a74dd6dc9d7b
natalicio almeida <natoctba@hotmail.com>
2024-11-24 11:26:10 -0300

    OrderItemPK

--- src/main/java/com/nato/dscommerce/entities/OrderItem.java
+++ src/main/java/com/nato/dscommerce/entities/OrderItem.java
@@ -25,4 +25,22 @@ public class OrderItem {
     public void setOrder(Order order) {
         id.setOrder(order);
     }
+    public Product getProduct() {
+        return id.getProduct();
+    }
+    public void setProduct(Product product) {
+        id.setProduct(product);
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
